d3695a2346b2522894f99812db61b2e00fb5175b
verzcar <carlo.verzeri@vecomentman.de>
2023-10-23 11:08:22 +0200

    added aws config

--- app/config/config.go
+++ app/config/config.go
@@ -156,6 +156,12 @@ func (c *Config) checkEnvironment() {
 		c.Aws.Auth.UserPoolId = os.Getenv("AWS_AUTH_USER_POOL_ID")
 		c.Aws.Auth.ClientSecret = os.Getenv("AWS_AUTH_CLIENT_SECRET")
 
+		c.Aws.S3.AccessKeyId = os.Getenv("AWS_S3_ACCESS_KEY")
+		c.Aws.S3.AccessKeySecret = os.Getenv("AWS_S3_ACCESS_SECRET_KEY")
+		c.Aws.S3.Region = os.Getenv("AWS_S3_REGION")
+		c.Aws.S3.BucketName = os.Getenv("AWS_S3_BUCKET_NAME")
+		c.Aws.S3.DefaultBaseURL = os.Getenv("AWS_S3_DEFAULT_BASE_URL")
+
 		c.Db.Host = os.Getenv("DB_HOST")
 		c.Db.Name = os.Getenv("DB_NAME")
 		c.Db.User = os.Getenv("DB_USER")
